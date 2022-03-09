9862f7669a7848f9c057a5ba738e167455a7e688
Denis Batista <denis.si2011@gmail.com>
2022-03-09 09:26:08 -0300

    FIX: Treinando.

--- codigoSemErro.js
+++ codigoSemErro.js
@@ -4,23 +4,3 @@ do {
     console.log(contador);
     contador--;
 } while (contador > 0);
-
-
-// const produtos = ['Sangung x40', 'iPhone X', 'HD SSD'];
-
-// for of(const produto, produtos) {
-//     console.log(produto);
-// }
-
-
-// const tipoUsuario = 'Supervisor';
-// let salario = 2500;
-
-// switch {
-//     case tipoUsuario = 'Admin':
-//     console.log(salario * 1.2);
-//     break;
-//     default:
-//         console.log(salario * 1.1);
-//     break;
-// }
