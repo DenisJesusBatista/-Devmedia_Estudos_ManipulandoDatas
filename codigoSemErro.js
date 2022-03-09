1c520ac136aba55278663992d2eb013bbce7b056
Denis Batista <denis.si2011@gmail.com>
2022-03-09 08:23:17 -0300

    Treinando manipulacao de data.

---
+++ codigoSemErro.js
@@ -0,0 +1,26 @@
+let contador = 10;
+
+do {
+    console.log(contador);
+    contador--;
+} while (contador > 0);
+
+
+// const produtos = ['Sangung x40', 'iPhone X', 'HD SSD'];
+
+// for of(const produto, produtos) {
+//     console.log(produto);
+// }
+
+
+// const tipoUsuario = 'Supervisor';
+// let salario = 2500;
+
+// switch {
+//     case tipoUsuario = 'Admin':
+//     console.log(salario * 1.2);
+//     break;
+//     default:
+//         console.log(salario * 1.1);
+//     break;
+// }
---
+++ imprimirData2.js
@@ -0,0 +1,5 @@
+const data = new Date();
+
+console.log('Dia do ano ' + data.getFullYear());
+console.log('Mes ' + data.getMonth());
+console.log('Horas ' + data.getHours());
