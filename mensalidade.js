d2f0a7ffe3d80db892d56dec5116b4b96e7e0482
Denis Batista <denis.si2011@gmail.com>
2022-03-17 10:31:24 -0300

    FIX: Treinando data.

---
+++ mensalidade.js
@@ -0,0 +1,14 @@
+const serie = 8;
+let mensalidade = 0;
+
+if (serie <= 3) {
+    mensalidade = 1000;
+}
+else if (serie <= 6) {
+    mensalidade = 1200;
+}
+else {
+    mensalidade = 1300;
+}
+
+console.log('R$ ' + mensalidade);
