67d8a28452097874b35c14191a662e0d98c826ac
Denis Batista <denis.si2011@gmail.com>
2022-03-20 10:46:46 -0300

    FIX: Treinando JS.

---
+++ ingredientes.js
@@ -0,0 +1,11 @@
+/*0 - açúcar
+1 - farinha de trigo
+2 - ovo
+*/
+
+const ingredientes = ['açúcar', 'farinha de trigo', 'ovo'];
+
+for (const index in ingredientes) {
+
+    console.log(index + ' - ' + ingredientes[index]);
+}
