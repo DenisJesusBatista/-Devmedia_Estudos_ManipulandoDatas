554b7ab42ef75bccc89f979fa7e2185e5bc0d591
Denis Batista <denis.si2011@gmail.com>
2022-03-22 09:02:34 -0300

    FIX: Treinando JS.

--- imprimirData2.js
+++ imprimirData2.js
@@ -1,5 +1,6 @@
 const data = new Date();
 
+//console.log('Dia do ano ' + data.getFullYear());
 console.log('Dia do ano ' + data.getFullYear());
 console.log('Mes ' + data.getMonth());
 console.log('Horas ' + data.getHours());
