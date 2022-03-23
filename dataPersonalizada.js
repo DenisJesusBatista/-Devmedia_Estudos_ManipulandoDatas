77210c82d82adb4a2bb734b4a3fad44c994a5eaa
Denis Batista <denis.si2011@gmail.com>
2022-03-23 08:23:52 -0300

    FIX: Imprimndo data personalizada.

---
+++ dataPersonalizada.js
@@ -0,0 +1,14 @@
+const dataAtual = new Date();
+
+const options = {
+
+    weekday: 'long',
+
+    year: "numeric",
+
+    month: 'numeric',
+
+    day: '2-digit',
+};
+
+console.log(dataAtual.toLocaleDateString('en-US', options));
